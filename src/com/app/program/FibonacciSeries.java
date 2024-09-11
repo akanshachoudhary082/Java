45e2d849e8e02f05d3c0c902cc72f42cb1a609e0
Akansha Choudhary <akanshachoudhary082@gmail.com>
2024-09-11 11:23:50 +0530

    Program

---
+++ src/com/app/program/FibonacciSeries.java
@@ -0,0 +1,24 @@
+package com.app.program;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		int n = 15;
+		int a = 0, b = 1;
+		
+		
+		System.out.print(a + " , " + b + ", ");
+		
+		int nextTerm ;
+		
+		for(int i=2;i<n;i++)
+		{
+			nextTerm = a+b;
+			a = b;
+			b = nextTerm;
+			System.out.print(nextTerm + " , ");
+		}
+         
+	}
+
+}
