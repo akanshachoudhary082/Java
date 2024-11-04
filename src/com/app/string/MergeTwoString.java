627201f5973607d51462bf79bdb811d4979cc4fc
Akansha Choudhary <akanshachoudhary082@gmail.com>
2024-11-04 17:53:35 +0530

    Program

--- src/com/app/string/MergeTwoString.java
+++ src/com/app/string/MergeTwoString.java
@@ -2,12 +2,48 @@ package com.app.string;
 
 public class MergeTwoString {
 
+//	public static void main(String[] args)
+//	{
+//		String s1 = "Hello Beautiful ";
+//		String s2 = "World";
+//		
+//		String merge = s1  + s2;
+//		
+//		System.out.println(merge);
+//	}
+	
+	//=======================using string builder ========================
+	
+	
+//	public static void main(String[] args)
+//	{
+//		String [] arr = {"Hello","Beatiful","World"};
+//		StringBuilder merge = new StringBuilder();
+//
+//		
+//		
+//		for(int i = 0;i< arr.length ; i++)
+//		{
+//			
+//			merge.append(arr[i]);
+//			
+//			if(i < arr.length - 1)
+//			{
+//				merge.append(" ");
+//			}
+//		}
+//		
+//		System.out.println(merge.toString());
+//	}
+	
+	
+	//=====================usig join method ===================
+	
 	public static void main(String[] args)
 	{
-		String s1 = "Hello Beautiful ";
-		String s2 = "World";
+		String[] arr = {"Hello" ,"Beautiful","world"};
 		
-		String merge = s1  + s2;
+		String merge =String.join(" ", arr);
 		
 		System.out.println(merge);
 	}
